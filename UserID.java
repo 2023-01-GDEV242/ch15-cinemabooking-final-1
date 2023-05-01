5060391afff12bce7245c8dc7697f17e74f0fae6
GhengisDon <98118019+GhengisDon@users.noreply.github.com>
2023-05-01 13:39:52 -0400

    update 1.5.23 13h39m
    13h39m 1.5.23
    -update of cinemabookingproject
    -update of userID

--- UserID.java
+++ UserID.java
@@ -10,25 +10,52 @@ public class UserID extends CinemaBookingSystem
     // instance variables - replace the example below with your own
     private String userFirst; //this is user first name
     private String userLast; //this is user last name 
-    private Date dob; //this is users date of birth
+    private String Contactnumber; //this is the contact number of the person
     
     /**
      * Constructor for objects of class UserID
      */
-    public UserID(String userFirst,String userLast,Date dob)
+    
+    public UserID()
+    {
+        // assign null values to 
+        userFirst=null;
+        userLast=null;
+        Contactnumber=null;
+    }
+    
+    public void UserID(String newFirst,String newLast,String Contactnumber)
     {
         // initialise instance variables
+        userFirst=newFirst;
+        userLast=newLast;
+        Contactnumber=newContactnumber;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    public String getuserFirst()
     {
-        // put your code here
-        return x + y;
+        return userFirst; //Store the first name of the user
+    }
+    
+    public String getuserLast()
+    {
+        return userLast; //Store the last name of the user
+    }
+    
+    public String getContactnumber()
+    {
+        return Contactnumber; //Store the phone number input of the user
+    }
+    
+    /**
+     * the following section is the print section which if everything works correctly will print on screen both the first and last name of the user as well as their given contact number
+     */
+    
+    public void print()
+    {
+        System.out.println("Thanks for experiencing with us today!");
+        System.out.println(userFirst+userLast);
+        System.out.println("Any official messages will go to ");
+        System.out.println(Contactnumber);
     }
 }
